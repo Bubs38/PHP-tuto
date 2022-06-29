25f54747ce964b5cc79ec0ab82f8c2a2cd15a302
Bubs38 <burgain.aurelien@gmail.com>
2022-06-29 18:14:49 +0200

    Version Cours 26

---
+++ class/Compteur.php
@@ -0,0 +1,35 @@
+<?php
+class Compteur {
+
+  const INCREMENT = 1;
+
+  private $fichier;
+
+  public function __construct(string $fichier)
+  {
+    $this->fichier = $fichier;
+  }
+
+
+  public function incrementer(): void 
+  {
+
+    $compteur = 1;
+    if (file_exists($this->fichier)) {
+        $compteur = (int)file_get_contents($this->fichier);
+        $compteur += static::INCREMENT;
+    }
+    file_put_contents($this->fichier, $compteur);
+}
+  
+
+  public function recuperer (): int
+  {
+
+    if (!file_exists($this->fichier)) {
+      return 0;
+    }
+    return file_get_contents($this->fichier);
+
+  }
+}
